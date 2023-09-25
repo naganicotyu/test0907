376979955745429f61f97f4f31c046c8434fba3b
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-09-25 14:31:14 +0900

    navitop

---
+++ layouts/navivarTop.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="navivar">
+    <v-app-bar
+  color="orange"
+  dense
+  elevation="3"
+  outlined
+  prominent
+  rounded
+  shaped
+    
+    >
+    
+        
+    </v-app-bar>
+    <Nuxt />
+    
+    </div>
+    
+    
+    
+    
+    </template>
+    
+    <script>
+    
+    </script>

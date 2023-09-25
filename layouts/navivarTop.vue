66175af6939295b9ec5de6f770ed37cf36475233
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-09-25 17:12:53 +0900

    homebosy

--- layouts/navivarTop.vue
+++ layouts/navivarTop.vue
@@ -10,6 +10,22 @@
   shaped
     
     >
+    <v-list>
+<v-list-item
+v-for="([icon,text],i) in items"
+:key="i"
+link
+>
+<v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+
+</v-list-item>
+    </v-list>
     
         
     </v-app-bar>
@@ -23,5 +39,15 @@
     </template>
     
     <script>
+    export default {
+    data: () => ({
+      items: [
+        ['mdi-email', 'Inbox'],
+        ['mdi-account-supervisor-circle', 'Supervisors'],
+        ['mdi-clock-start', 'Clock-in'],
+      ],
+    }),
+  }
+
     
     </script>

f679e00e86371a34997d80f9b2f074cb15f7a549
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-09-27 17:30:40 +0900

    3

--- components/home-Introduction.vue
+++ components/home-Introduction.vue
@@ -1,9 +1,42 @@
 
 <template>
 
+<v-carousel
+dark="true"
+>
+<v-carousel-item>
+    v-for="(image,i) in items"
+    :key="i"
+    :src="image.src"
+
+</v-carousel-item>
+</v-carousel>
 </template>
 
+
+
 <script>
+
+export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
 
 </script>
 
